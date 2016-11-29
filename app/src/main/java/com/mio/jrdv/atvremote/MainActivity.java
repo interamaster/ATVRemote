04df6fd8d6eb7e92d49a5c9089b8955c018e3cc2
joseramondelgadoMACPRO <interamaster@gmail.com>
2016-11-29 23:58:40 +0100

    v02 CAMBIADO XML A MANDO ORIGINLA APPLE TV NO FUNCIONA MENU Y DERECHA LO DETCTA EN CUALQUIER SITIO FURA DE LOS BOTONES

--- app/src/main/java/com/mio/jrdv/atvremote/MainActivity.java
+++ app/src/main/java/com/mio/jrdv/atvremote/MainActivity.java
@@ -1,19 +1,22 @@
 package com.mio.jrdv.atvremote;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.hardware.ConsumerIrManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
 
     ConsumerIrManager mCIR ;
     private SparseArray<String> irData;
@@ -21,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_old_remote);
 
         //initializae:
 
@@ -31,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         PackageManager pm = getPackageManager();
         Log.e("INFO","pm.hasSystemFeature(PackageManager.FEATURE_CONSUMER_IR): " +
                 pm.hasSystemFeature(PackageManager.FEATURE_CONSUMER_IR));
+
+
+        //INITILIZE DE LA IMAGEN PARA DETCET BUTTONS
+
+        ImageView iv = (ImageView) findViewById (R.id.image);
+        if (iv != null) {
+            iv.setOnTouchListener (this);
+        }
 
 
         //rellenamos los valores de cada button
@@ -103,13 +114,19 @@ public class MainActivity extends AppCompatActivity {
          0E40    //hasta 110ms en off  (110-11.8125 ms=96,12 -->3648 -->0E40
          ..
          y asi hasta que se suelte
-         //de moento simulo que se deja pulsado 10 ciclos...
+         //de moento simulo que se deja pulsado 10 ciclos...pero no funciona..
 
 
 
          */
 
-        Button selectButton=(Button)findViewById(R.id.buttonSelect);
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////LONG TOUCH..PTE//////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+       /* Button selectButton=(Button)findViewById(R.id.buttonSelect);
 
         selectButton.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
@@ -144,10 +161,159 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+*/
 
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////// ///////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     }
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////touch listener///////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent ev) {
+
+
+        boolean handledHere = false;
+
+        final int action = ev.getAction();
+
+        final int evX = (int) ev.getX();
+        final int evY = (int) ev.getY();
+        int nextImage = -1;			// TODO posible usar para efecto irda ?¿?¿resource id of the next image to display
+
+        // If we cannot find the imageView, return.
+        ImageView imageView = (ImageView) v.findViewById (R.id.image);
+        if (imageView == null) return false;
+
+        // When the action is Down, see if we should show the "pressed" image for the default image.
+        // We do this when the default image is showing. That condition is detectable by looking at the
+        // tag of the view. If it is null or contains the resource number of the default image, display the pressed image.
+        Integer tagNum = (Integer) imageView.getTag ();
+        int currentResource = (tagNum == null) ? R.drawable.remote_orig_2 : tagNum.intValue ();
+
+        // Now that we know the current resource being displayed we can handle the DOWN and UP events.
+
+        switch (action) {
+            case MotionEvent.ACTION_DOWN :
+                if (currentResource == R.drawable.remote_orig_2) {
+                    //TODO nextImage = R.drawable.p2_ship_pressed;
+                    handledHere = true;
+       /*
+       } else if (currentResource != R.drawable.p2_ship_default) {
+         nextImage = R.drawable.p2_ship_default;
+         handledHere = true;
+       */
+                } else handledHere = true;
+                break;
+
+            case MotionEvent.ACTION_UP :
+                // On the UP, we do the click action.
+                // The hidden image (image_areas) has three different hotspots on it.
+                // The colors are red, blue, and yellow.
+                // Use image_areas to determine which region the user touched.
+                int touchColor = getHotspotColor (R.id.image_areas, evX, evY);
+
+                // Compare the touchColor to the expected values. Switch to a different image, depending on what color was touched.
+                // Note that we use a Color Tool object to test whether the observed color is close enough to the real color to
+                // count as a match. We do this because colors on the screen do not match the map exactly because of scaling and
+                // varying pixel density.
+                ColorTool ct = new ColorTool ();
+                int tolerance = 25;
+                //TODO nextImage = R.drawable.p2_ship_default;
+
+                if (ct.closeMatch (Color.CYAN, touchColor, tolerance)) {
+                    //TODO nextImage = R.drawable.p2_ship_alien;
+                    Log.d("INFO", "pulsado arriba");
+
+                }
+                else if (ct.closeMatch (Color.GREEN, touchColor, tolerance))
+
+                {
+                   //TODO  nextImage = R.drawable.p2_ship_powered;
+                    Log.d("INFO", "pulsado abajo");
+
+                }
+                else if (ct.closeMatch (Color.BLACK, touchColor, tolerance))
+                {
+                    //TODO nextImage = R.drawable.p2_ship_no_star;
+                    Log.d("INFO", "pulsado derecha");
+                }
+                else if (ct.closeMatch (Color.YELLOW, touchColor, tolerance))
+                {
+                   //TODO  nextImage = R.drawable.p2_ship_default;
+
+                    Log.d("INFO", "pulsado izqda");
+                }
+
+                else if (ct.closeMatch (Color.WHITE, touchColor, tolerance))
+                {
+                    //TODO nextImage = R.drawable.p2_ship_default;
+
+                    Log.d("INFO", "pulsado play/pause");
+                }
+
+                else if (ct.closeMatch (Color.MAGENTA, touchColor, tolerance))
+                {
+                    //TODO nextImage = R.drawable.p2_ship_default;
+                    Log.d("INFO", "pulsado menu");
+                }
+
+
+                // If the next image is the same as the last image, go back to the default.
+                // toast ("Current image: " + currentResource + " next: " + nextImage);
+                if (currentResource == nextImage) {
+                    nextImage = R.drawable.remote_orig_2;
+                }
+                handledHere = true;
+                break;
+
+            default:
+                handledHere = false;
+        } // end switch
+
+        if (handledHere) {
+
+            if (nextImage > 0) {
+                imageView.setImageResource (nextImage);
+                imageView.setTag (nextImage);
+            }
+        }
+        return handledHere;
+    }
+
+
+
+    /**
+     * Get the color from the hotspot image at point x-y.
+     *
+     */
+
+    public int getHotspotColor (int hotspotId, int x, int y) {
+        ImageView img = (ImageView) findViewById (hotspotId);
+        if (img == null) {
+            Log.d ("ImageAreasActivity", "Hot spot image not found");
+            return 0;
+        } else {
+            img.setDrawingCacheEnabled(true);
+            Bitmap hotspots = Bitmap.createBitmap(img.getDrawingCache());
+            if (hotspots == null) {
+                Log.d ("ImageAreasActivity", "Hot spot bitmap was not created");
+                return 0;
+            } else {
+                img.setDrawingCacheEnabled(false);
+                return hotspots.getPixel(x, y);
+            }
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////CONVERTER DE HEX A INT DE INT A STRING//////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     protected String hex2dec(String irData) {
         List<String> list = new ArrayList<String>(Arrays.asList(irData
@@ -230,6 +396,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////BUTTON DETECT OLD XML///////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     public void downButton(View view) {
 
         String data = irData.get(view.getId());
@@ -392,4 +562,6 @@ public class MainActivity extends AppCompatActivity {
             mCIR.transmit(38028,numbers);
         }
     }
+
+
 }
